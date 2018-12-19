9e8ff30d12c7ab4d6cdfc0680dd970666c1f33b6
mcobb98 <43396792+mcobb98@users.noreply.github.com>
2018-12-19 15:39:10 -0500

    Add files via upload

---
+++ follow.py
@@ -0,0 +1,60 @@
+# SYSTEM IMPORT
+import time
+import sys
+import os
+import random as rand 
+
+# SELENIUM 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+
+
+# WEBDRIVER OPTIONS
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+driver = webdriver.Chrome(executable_path="/home/matt/TwitterBot/chromedriver", chrome_options=options)
+#wait = WebDriverWait(driver, 10)
+
+def randpharm():
+	pharmas = ["pfizer", "abbvie", "sanofi", "JNJCares", "Celgene", "Roche"]
+	return random.choice(pharmas)
+
+# WAIT FUNCTION
+def wait():
+	time.sleep(rand.randint(3,60))
+
+# LOGIN WITH TWITTER
+def login():
+    driver.get("https://twitter.com/login")
+    #wait()
+    username = driver.find_element_by_class_name("js-username-field")
+    username.clear()
+    username.send_keys('LandosBio')
+    password = driver.find_element_by_class_name("js-password-field")
+    password.clear()
+    password.send_keys('newLandosTwitter2018')
+    password.submit()
+
+# FOLLOW BOT
+def followbot():
+        driver.get("https://twitter.com/" + randpharm() + "followers")
+        buttons = get_elements_by_class_name(follow-text)
+        for btn in buttons[1:50]:
+        		driver.execute_script("arguments[0].click();", btn)
+        		wait()
+    
+
+
+
+# ACTIVITY
+login()
+wait()
+followbot()
+driver.quit()
+sys.exit()
+
+    
+
+
